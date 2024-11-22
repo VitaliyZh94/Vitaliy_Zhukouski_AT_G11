6f1d80885518e78282493b3a6c0bbf280ed02424
user <zhukouski.vitaliy@gmail.com>
2024-11-22 22:49:00 +0300

    day4, bubble task completed

--- src/project/boxing/Bottle.java
+++ src/project/boxing/Bottle.java
@@ -25,20 +25,31 @@ public class Bottle {
     }
 
     public void open() {
-        this.water.isOpened();
+        System.out.println("Bottle is opened");
+
+        water.isOpened();
+        water.setOpened();
+
+        System.out.println("\n");
     }
 
     public void warm(int temperature) {
         water.setTemperature(temperature);
+        System.out.printf("Set %d temperature to water\n", temperature);
     }
 
     public void setBubbles(double liters) {
 
-        int arrayLength = 10000 * (int) liters;
-        //генерирует массив из обьектов Bubble
-        Bubble[] bubbles = new Bubble[arrayLength];
+        double arrayLength = 10000 * liters;
+
+        Bubble[] bubbles = new Bubble[(int)arrayLength];
         for (int i = 0; i < bubbles.length; i++) {
             bubbles[i] = new Bubble("gas");
         }
+
+        System.out.printf("Set %f bubbles to the bottle\n", arrayLength);
+
+        water.pump(bubbles);
+
     }
 }
