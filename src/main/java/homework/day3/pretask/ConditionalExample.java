e3bb01dbb5318b9e8303694ea993304389072283
user <zhukouski.vitaliy@gmail.com>
2024-12-18 23:23:11 +0300

    refactoring project, add selenium task

---
+++ src/main/java/homework/day3/pretask/ConditionalExample.java
@@ -0,0 +1,13 @@
+package homework.day3.pretask;
+
+public class ConditionalExample {
+
+    public static void main(String[] args) {
+        int num = 15;
+
+        if (num > 10) {
+            System.out.println(num + " > 10");
+        } else
+            System.out.println(num + " < 10");
+    }
+}
---
+++ src/main/java/homework/day3/pretask/IncrementDecrementExample.java
@@ -0,0 +1,14 @@
+package homework.day3.pretask;
+
+public class IncrementDecrementExample {
+    public static void main(String[] args) {
+        int variable = 3;
+        variable++;
+
+        System.out.println(variable);
+
+        variable--;
+
+        System.out.println(variable);
+    }
+}
